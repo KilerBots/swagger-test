113f0a275ed7f829226e90206e94b476c496f53c
KilerBots <davidmersey707@gmail.com>
2025-07-19 07:50:55 +0800

    Update swagger.js

--- pages/api/swagger.js
+++ pages/api/swagger.js
@@ -7,7 +7,7 @@ const options = {
       title: 'Rest API',
       version: '1.0.0',
     },
-    servers: [{ url: 'http://localhost:3000' }],
+    servers: [{ url: '/api' }],
   },
   apis: ['pages/api/**/*.js'],
 };
